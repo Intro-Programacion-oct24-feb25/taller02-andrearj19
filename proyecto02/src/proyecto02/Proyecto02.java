f984da09fcaf16139b1bbd69bf7281a58fa701e2
Andrea Arias <aaarias19@utpl.edu.ec>
2024-10-17 11:49:16 -0500

    tercer commit,trabajando con Git y Githhubclassroom

---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto02;
+
+/**
+ *
+ * @author utpl
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ejemplo dos básico de Java versionado a traves de Git manejado desde Github ademas usando Githhubclassroom");
+        
+                
+
+    }
+    
+}
